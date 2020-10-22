38f96f5da3595518dfd290f603fbb318cd0eeca7
adsalomo <adsalomo@gmail.com>
2020-10-21 20:15:37 -0500

    feat: codigo inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  FormControl,
+  FormGroup,
+  FormLabel,
+} from "react-bootstrap";
+import "./App.css";
+import axios from "axios";
+const downloader = require("js-file-download");
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:8082",
+});
 
 function App() {
+  const [id, setId] = useState(null);
+
+  const handleGenerateFile = async () => {
+    try {
+      const resp = await axiosInstance.post(`/report`, {
+        paramTitle: "Titulo 2ccccccccc cccccccccccc cccccccccccccc",
+      });
+      setId(resp.data.reportId);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleShowFile = async () => {
+    try {
+      const resp = await axiosInstance.get(`/report`, {
+        params: { id },
+        responseType: "arraybuffer",
+      });
+      downloader(resp.data, `${new Date().getTime()}.pdf`);
+    } catch (error) {
+      const res = JSON.parse(Buffer.from(error.response.data).toString("utf8"));
+      alert(res.message);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid="md">
+      <Row>
+        <Col lg={12} xs={12}>
+          <FormGroup>
+            <Button variant="primary" onClick={handleGenerateFile}>
+              Generate file
+            </Button>
+          </FormGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={3} xs={3}>
+          <FormGroup>
+            <FormLabel>Id</FormLabel>
+            <FormControl disabled={true} value={id != null ? id : id} />
+          </FormGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={3} xs={3}>
+          <FormGroup>
+            <Button variant="primary" onClick={handleShowFile}>
+              Ver PDF
+            </Button>
+          </FormGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
